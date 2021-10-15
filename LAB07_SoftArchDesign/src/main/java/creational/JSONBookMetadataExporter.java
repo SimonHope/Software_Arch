db26436369b20070f5085d0d08accd0ee461dc44
Simon Hope <35981689+SimonHope@users.noreply.github.com>
2021-10-15 20:40:36 +0700

    LAB07

---
+++ LAB07_SoftArchDesign/src/main/java/creational/JSONBookMetadataExporter.java
@@ -0,0 +1,8 @@
+package creational;
+
+public class JSONBookMetadataExporter extends BookMetadataExporter {
+    @Override
+    public BookMetadataFormatter createBookExport() {
+        return new JSONBookMetadataFormatter();
+    }
+}
